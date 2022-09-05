6fa64548ce5671bd706d9424f6756f8dadf3fce1
Gourang Jain <gourangjain7@gmail.com>
2022-09-05 17:28:11 +0530

    added animations

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ window.onbeforeunload = function () {
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
-
+ 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute('data-visible')
    ? navToggle.setAttribute('aria-expanded', false)
